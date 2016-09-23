eb4a3f1f7e7a35924f20a5e245f4d9ad1b7fde03
YuAn <atwood12@gmail.com>
2016-09-23 03:04:56 +0000

    finalize client side

--- src/app/modules/absForm/absForm.constant.js
+++ src/app/modules/absForm/absForm.constant.js
@@ -23,10 +23,11 @@ const absFormConstant = {
         affiliations: ["Department of Biophysics, University of Texas Southwestern Medical Center, Dallas, TX","Department of Radiology, University of Texas Southwestern Medical Center, Dallas, TX"],
         useAffiliationSup: true,
         keywords: "keyword, are, seperated, by, comma",
-        absContent: "Please read on before you delete the following and paste in your abstract.\n\nIf the bottom part of your abstract doesn't show up, that indicates the content is _too long_. Please trim it. While we are very open to the format, it must be single page print out.\n\nFor multiple paragraphs, place an empty line between every paragraph. So that they will be separated nicely.\n\nMarkdown runs in this box. _Italic text_ and **bold text** are made possible.",
+        absContent: "Please read on before you delete the following and paste in your abstract.\n\nIf the bottom part of your abstract doesn't show up, that indicates the content is _too long_. Please shorten it. While we are very open to the format, it must be single page print out.\n\nFor multiple paragraphs, place an empty line between every paragraph. So that they will be separated nicely.\n\nMarkdown runs in this box. _Italic text_ and **bold text** are possible. Use single underscore to wrap _italic text_, and double asterisk to wrap **bold text**",
         createdAt: new Date(),
         updatedAt: new Date(),
-        submittedAt: []
+        submittedAt: [],
+        canGiveATalk: Boolean,
     },
     
 };
