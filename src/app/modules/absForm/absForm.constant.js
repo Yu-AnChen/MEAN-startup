17b81a6841f0e8d5a309077e7f3c80df4460a3d6
YuAn <atwood12@gmail.com>
2016-09-25 02:26:08 +0000

    edit new abstract const

--- src/app/modules/absForm/absForm.constant.js
+++ src/app/modules/absForm/absForm.constant.js
@@ -22,7 +22,7 @@ const absFormConstant = {
         authors: [{"name":"Siao-Ming Wang","role":"Presenting","affiliationSup":[1],"affiliationOfAuthor":["Department of Biophysics, University of Texas Southwestern Medical Center, Dallas, TX"],"validAuthor":true},{"name":"Jane Doe","role":"Corresponding","affiliationSup":[2],"affiliationOfAuthor":["Department of Radiology, University of Texas Southwestern Medical Center, Dallas, TX"],"validAuthor":true}],
         affiliations: ["Department of Biophysics, University of Texas Southwestern Medical Center, Dallas, TX","Department of Radiology, University of Texas Southwestern Medical Center, Dallas, TX"],
         useAffiliationSup: true,
-        keywords: "keyword, are, seperated, by, comma",
+        keywords: "keywords, are, seperated, by, comma",
         absContent: "Please read on before you delete the following and paste in your abstract.\n\nIf the bottom part of your abstract doesn't show up, that indicates the content is _too long_. Please shorten it. While we are very open to the format, it must be single page print out.\n\nFor multiple paragraphs, place an empty line between every paragraph. So that they will be separated nicely.\n\nMarkdown runs in this box. _Italic text_ and **bold text** are possible. Use single underscore to wrap _italic text_, and double asterisk to wrap **bold text**",
         createdAt: new Date(),
         updatedAt: new Date(),
