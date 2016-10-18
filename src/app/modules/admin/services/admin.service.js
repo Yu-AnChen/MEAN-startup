820f1019ed37c24184188807f21b198b61f9e859
YuAn <atwood12@gmail.com>
2016-10-18 06:17:14 +0000

    preliminary backoffice add .docx file name

--- src/app/modules/admin/services/admin.service.js
+++ src/app/modules/admin/services/admin.service.js
@@ -1,6 +1,29 @@
 /* @ngInject */
 class adminService {
     constructor() {
+        this.data = {
+            // something: 'string'
+        };
+    }
+    // public api
+    cleanup(){
+        this.data = null;
+    }
+    storeData(data){
+        this.data = data;
+        console.log(this.data);
+        return;
+    }
+    getData(){
+        let variable = 123123123123;
+        const var2 = 'dasdasd';
+        console.log(this.data);
+        console.log(this);
+        return this.data;
+    }
+    // internal use only
+    _formatData(){
+        this.data == data;
     }
 }
 
