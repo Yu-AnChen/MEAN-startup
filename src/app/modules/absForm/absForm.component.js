1528b541fc1aa84d26ee4735b1c564c5b3fce541
Yu-An Chen <atwood12@gmail.com>
2017-11-15 08:22:21 -0600

    Cleanup  console.log

--- src/app/modules/absForm/absForm.component.js
+++ src/app/modules/absForm/absForm.component.js
@@ -10,9 +10,9 @@ const absFormComponent = {
     controller: /* @ngInject */
     class absFormInputController {
         static get $inject() {
-            return ['$log', '$timeout', '$scope', '$interval', 'FormApi', 'UserApi', '$state', 'focus', '$stateParams', 'absFormService', '$mdMedia'];
+            return ['$log', '$timeout', '$scope', '$interval', 'FormApi', 'UserApi', '$state', 'focus', '$stateParams', 'absFormService', '$mdMedia', 'systemSettingService'];
         }
-        constructor($log, $timeout, $scope, $interval, FormApi, UserApi, $state, focus, $stateParams, absFormService, $mdMedia) {
+        constructor($log, $timeout, $scope, $interval, FormApi, UserApi, $state, focus, $stateParams, absFormService, $mdMedia, systemSettingService) {
             this.$log = $log;
             this.$timeout = $timeout;
             this.$scope = $scope;
@@ -24,6 +24,7 @@ const absFormComponent = {
             this.$stateParams = $stateParams;
             this.absFormService = absFormService;
             this.$mdMedia = $mdMedia;
+            this.systemSettingService = systemSettingService;
         }
         $onInit() {
             this.absFormConst = absFormConst;
@@ -40,6 +41,7 @@ const absFormComponent = {
             this.AuthorAffilTemplate = this.genAuthorAffilTemplate();
             this.absWithinPage = true;
             this.submitBtnClicked = false;
+            this.systemSettingService.getSystemSetting().then((data) => this.isBeforeDeadline = data.isBeforeDeadline);
             // this.submittedInTime = false;
         }
         $onDestroy() {
@@ -47,13 +49,11 @@ const absFormComponent = {
         }
         
         detectAbsOverflowY(callback) {
-            // console.log("detectAbsOverflowY");
-            var el = document.querySelector("#abs_print");
-            // console.log(el.innerHTML);
+            var el = document.querySelector('#abs_print');
             if (el.scrollHeight > el.clientHeight) {
-               this.absWithinPage = false; 
+                this.absWithinPage = false; 
             } else { this.absWithinPage = true; }
-            if (typeof callback === "function") {
+            if (typeof callback === 'function') {
                 callback();
             }
         }
@@ -68,12 +68,9 @@ const absFormComponent = {
             });
         }
         getCurrentUser() {
-            console.log('get currentuser from server');
             this.UserApi.getCurrentUser().then((res)=>{
-                // console.log(res.data);
                 if (res.data.email) {
                     // this.$state.go('app.absForm', {email: res.data.email});
-                    // console.log(res.data.email);
                     this.currentUser = res.data;
                     // this.form.email = res.data.email;
                 } else { 
@@ -95,13 +92,9 @@ const absFormComponent = {
         //     return data;
         // }
         getAbstract(email, title) {
-            console.log('get abstract from server');
             this.FormApi.get(email, title).then((res)=>{
-                // console.log(this.form._id);
                 this.form = res.data[0];
                 this._submittedInTime(res.data[0]);
-                // console.log(this.convertNullToNaN(res.data[0]))
-                // console.log(this.form._id);
             }, ()=>{
                 console.log('error: getAbstract');
                 this.form = this.absFormConst.abstractNewUser;
@@ -109,15 +102,12 @@ const absFormComponent = {
             });
         }
         submit() {
-            console.log("submitting");
-            console.log(this.form);
             this.submitBtnClicked = true;
             this.form.canGiveATalk = true;
             this.form.submittedAt[0] = new Date();
             this.fillInAuthorAndAffil(()=>{
                 this.decideUseAffiliationSup(()=>{
                     this.detectAbsOverflowY(()=>{
-                        console.log(this.absWithinPage);
                         if (this.absWithinPage) { 
                             this.saveToDatabase(true); 
                         } 
@@ -126,30 +116,26 @@ const absFormComponent = {
             });
         }
         saveToDatabase(submit){
-            console.log("save To Database");
             this.form.email = this.currentUser.email;
             this.form.updatedAt = new Date();
             this.FormApi.save(this.form).then((res)=>{
-                console.log(res);
                 if (res.data.ops) {
                     this.getAbstract(res.data.ops[0].email, res.data.ops[0].title);
                 }
                 if (submit) {
                     this.generatePdfFile(this.currentUser.email);
-                    // this.$state.go('app.absSubmitComplete')
                 }
             }, ()=>{
                 console.log('error: saveToDatabase');
             });
         }
         generatePdfFile(email) {
-            var el = document.querySelector("abs-print");
+            var el = document.querySelector('abs-print');
             var data = {
                 email: email,
                 html: el.innerHTML
-            }
+            };
             this.absFormService.sendAbsPrintEl(data).then((res)=>{
-                // console.log(res);
                 this.$state.go('app.absSubmitComplete');
             }, ()=>{
                 console.log('error: generatePdfFile');
@@ -162,14 +148,14 @@ const absFormComponent = {
         // DATA STRUCTURE
         getBlankForm() {
             return {
-                email: "",
-                title: "",
-                field: "",
-                authors: [{"name":"","role":"","affiliationSup":[NaN],"affiliationOfAuthor":[""],"validAuthor":false},{"name":"","role":"","affiliationSup":[NaN],"affiliationOfAuthor":[""],"validAuthor":false}],
+                email: '',
+                title: '',
+                field: '',
+                authors: [{'name':'','role':'','affiliationSup':[NaN],'affiliationOfAuthor':[''],'validAuthor':false},{'name':'','role':'','affiliationSup':[NaN],'affiliationOfAuthor':[''],'validAuthor':false}],
                 affiliations: [],
                 useAffiliationSup: false,
-                keywords: "",
-                absContent: "",
+                keywords: '',
+                absContent: '',
                 createdAt: new Date(),
                 updatedAt: new Date(),
                 submittedAt: []
@@ -177,12 +163,12 @@ const absFormComponent = {
         }
         addAuthor() {
             let newAuthor = {
-                name: "",
+                name: '',
                 role: this.genAuthorAffilTemplate()[this.form.authors.length],
                 affiliationSup: [NaN],
-                affiliationOfAuthor: [""],
+                affiliationOfAuthor: [''],
                 validAuthor: false
-            }
+            };
             if (this.form.authors.length < 2) {
                 this.form.authors.push(newAuthor);
             } else {
@@ -190,7 +176,7 @@ const absFormComponent = {
             }
         }
         genAuthorAffilTemplate() {
-            let AuthorAffilTemplate = ["Presenting", "Corresponding", "General"];
+            let AuthorAffilTemplate = ['Presenting', 'Corresponding', 'General'];
             for (let i=0; i<27; i++) {
                 AuthorAffilTemplate.push(AuthorAffilTemplate[2]);
             }
@@ -207,7 +193,7 @@ const absFormComponent = {
         // TOOLS
         genUniqueArray(theArray) {
             let n = {},
-              r = [];
+                r = [];
             for (let i=0; i<theArray.length; i++) {
                 if (!n[theArray[i]]) {
                     n[theArray[i]] = true;
@@ -224,7 +210,6 @@ const absFormComponent = {
         // AUTHOR AND AFFILIATION
         // this.form.affiliations = [unique affiliations]
         checkAffilUnique() {
-            console.log('checkAffilUnique');
             let affilRaw = [];
             for (let i=0; i<this.form.authors.length; i++) {
                 affilRaw = affilRaw.concat(this.form.authors[i].affiliationOfAuthor);
@@ -255,14 +240,13 @@ const absFormComponent = {
                 this.form.authors[i].validAuthor = Boolean(
                     this.filterString(this.form.authors[i].name) ||
                     this.form.authors[i].affiliationOfAuthor.filter(this.filterString).length
-                    )
+                    );
             }
         }
         filterValidAuthor(author) {
             return author.validAuthor;
         }
         decideUseAffiliationSup(callback) {
-            console.log('decideUseAffiliationSup');
             let validAuthors = this.form.authors.filter(this.filterValidAuthor);
             if (validAuthors.length > 1 && this.form.affiliations.length > 1) {
                 this.form.useAffiliationSup = false;
@@ -279,7 +263,7 @@ const absFormComponent = {
             } else {
                 this.form.useAffiliationSup = false;
             }
-            if (typeof callback == "function") {
+            if (typeof callback == 'function') {
                 callback();
             }
         }
@@ -289,7 +273,6 @@ const absFormComponent = {
             for(let x=0; x<this.form.authors.length; x++){
                 this.filteredAuthors.push(this.form.authors[x].name);
             }
-            // console.log(this.filteredAuthors);
             for(let i=0; i<this.filteredAuthors.length; i++) {
                 const pos = this.filteredAuthors[i].indexOf(name);
                 if (pos > -1){
@@ -299,49 +282,47 @@ const absFormComponent = {
             this.filteredAuthors = newfilteredAuthors;
         }
         fillInAuthorAndAffil(callback) {
-            console.log('fillInAuthorAndAffil');
             for (let i=0; i<this.form.authors.length; i++) {
                 if (this.filterString(this.form.authors[i].name)) {
                     if (!this.form.authors[i].affiliationOfAuthor.filter(this.filterString).length) {
-                        this.form.authors[i].affiliationOfAuthor[0] = "Unknown Affiliation";
+                        this.form.authors[i].affiliationOfAuthor[0] = 'Unknown Affiliation';
                         this.checkAffilUnique();
                         this.matchAffilSup();
                     }
                 } else {
                     if (this.form.authors[i].affiliationOfAuthor.filter(this.filterString).length) {
-                        this.form.authors[i].name = "Unknown Author";
+                        this.form.authors[i].name = 'Unknown Author';
                     }
                 }
             }
             // this.decideUseAffiliationSup();
-            if (typeof callback == "function") {
+            if (typeof callback == 'function') {
                 callback();
             }
         }
         // FIELDS
         getFields() {
             return [
-                "Translational biology", 
-                "Developmental biology", 
-                "Neuroscience", 
-                "Bioinformatics", 
-                "Bioengineering", 
-                "Biostatistics", 
-                "Immunology", 
-                "Molecule biology", 
-                "Microbiology", 
-                "Genetics", 
-                "Biophysics", 
-                "Biochemistry", 
-                "Pharmacology"
-            ]
+                'Translational biology', 
+                'Developmental biology', 
+                'Neuroscience', 
+                'Bioinformatics', 
+                'Bioengineering', 
+                'Biostatistics', 
+                'Immunology', 
+                'Molecular biology', 
+                'Microbiology', 
+                'Genetics', 
+                'Biophysics', 
+                'Biochemistry', 
+                'Pharmacology'
+            ];
         }
         // DATABASE
         toggleAutoBackup() {
             if (this.autoBackupConfig.status) {
                 this.autoBackupConfig.msg = 'Changes saved';
                 this.autoBackup = this.$interval(()=>{
-                    // console.log('save');
                     this.saveToDatabase(false);
                 }, this.autoBackupConfig.time);
             } else {
@@ -357,23 +338,25 @@ const absFormComponent = {
             this.focus('author-name-'+(this.form.authors.length-2));
         }
         focusNewAffiliation(authorIndex) {
-            // console.log(authorIndex);
-            this.focus('author-'+authorIndex+'-affiliation-'+(this.form.authors[authorIndex].affiliationSup.length-1))
+            this.focus('author-'+authorIndex+'-affiliation-'+(this.form.authors[authorIndex].affiliationSup.length-1));
             // 'author-'+parentIndex+'-affiliation-'+(this.form.authors[parentIndex].affiliationSup.length-2)
         }
         setFooterClass(open) {
-            var footer = document.querySelector("footer div");
-            open ? footer.setAttribute("class", "p_relative") : 
-                footer.setAttribute("class", "p_absolute")
+            var footer = document.querySelector('footer div');
+            open 
+                ? footer.setAttribute('class', 'p_relative') 
+                : footer.setAttribute('class', 'p_absolute');
         }
         _submittedInTime(data) {
             const createdTime = Date.parse(data.createdAt);
-            const deadline = Date.parse("2017-10-01T05:30:00.000Z");
+            const deadline = Date.parse('2018-01-10T05:30:00.000Z');
             if (createdTime <= deadline) {
                 console.log('before deadline');
-                // return true;
                 this.submittedInTime = true;
-            } else { console.log('after deadline'); this.submittedInTime = false; }
+            } else { 
+                console.log('after deadline'); 
+                this.submittedInTime = false; 
+            }
             // if (this.form) {
             //     const createdTime = Date.parse(this.form.createdAt);
             //     const deadline = Date.parse("2016-10-17T22:57:36.910Z");
